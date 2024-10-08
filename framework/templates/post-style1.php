b0f2031230dad014bfc8451e3118eb435e336622
Duc Dung <ducdung@192.168.102.40>
2024-10-08 14:27:17 +0700

    section Appointment,section Opening Hours, Section Blog, Footer

--- framework/templates/post-style1.php
+++ framework/templates/post-style1.php
@@ -10,21 +10,22 @@ $category = get_the_terms($post_id, 'category');
       <div class="bt-post--category">
         <?php
         if (!empty($category)) {
-          echo  '<a href="'.get_category_link($category[0]->term_id).'">'.$category[0]->name.'</a>';
+          echo  '<a href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->name . '</a>';
         }
         ?>
       </div>
     </div>
     <div class="bt-post--content">
+      <?php
+      echo denali_post_publish_render();
+      echo denali_post_title_render();
+      ?>
       <div class="bt-post--info">
         <?php
-        echo denali_post_publish_render();
-
+        echo denali_author_icon_render();
+        echo denali_reading_time_render();
         ?>
       </div>
-      <?php echo denali_post_title_render();
-      echo denali_author_icon_render();
-      ?>
     </div>
 
   </div>
